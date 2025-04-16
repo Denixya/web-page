1cbe71a9074b71c51c15ff0e1bc8ac7a4b610a35
Denixya <denixyart@gmail.com>
2025-04-16 18:41:38 +0200

    feat: background image

--- src/app/components/wow-guild-card/wow-guild-card.component.ts
+++ src/app/components/wow-guild-card/wow-guild-card.component.ts
@@ -1,8 +1,11 @@
 import { Component, input } from '@angular/core';
+import { NgStyle } from '@angular/common';
 import { WowCharacter } from './models/wow-character.model';
 
 @Component({
+  standalone: true,
   selector: 'app-wow-guild-card',
+  imports: [NgStyle],
   templateUrl: './wow-guild-card.component.html',
   styleUrls: ['./wow-guild-card.component.scss'],
 })
