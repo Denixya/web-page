8da6e71f1412d7fd28db250ae450356579384d83
Denixya <denixyart@gmail.com>
2025-07-27 12:39:11 +0200

    feat: home cards animation and pokedex

---
+++ src/app/pages/project-pokedex/project-pokedex.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-pokedex',
+  imports: [],
+  templateUrl: './project-pokedex.component.html',
+  styleUrl: './project-pokedex.component.scss'
+})
+export class ProjectPokedexComponent {
+
+}
