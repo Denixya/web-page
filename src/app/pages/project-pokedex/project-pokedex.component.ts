b890050fddb82ce77226ac8df93b4d37881c6c84
Denixya <denixyart@gmail.com>
2025-08-21 17:48:09 +0200

    feat: implement PokeTable component

--- src/app/pages/project-pokedex/project-pokedex.component.ts
+++ src/app/pages/project-pokedex/project-pokedex.component.ts
@@ -1,13 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { CardComponent } from '../../components/card/card.component';
-import { PokedexService } from '../../services/pokedex-store.service';
+import { PokedexService } from '../../services/pokedex.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { catchError, of } from 'rxjs';
 import { LoadingMessageComponent } from '../../components/loading-message/loading-message.component';
+import { PokeTableComponent } from '../../components/poke-table/poke-table.component';
 
 @Component({
   selector: 'app-project-pokedex',
-  imports: [CardComponent, LoadingMessageComponent],
+  imports: [CardComponent, LoadingMessageComponent, PokeTableComponent],
   templateUrl: './project-pokedex.component.html',
   styleUrl: './project-pokedex.component.scss',
 })
